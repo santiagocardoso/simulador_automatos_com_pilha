58dfe08c4c31dd75aa311022c5328008efc32d43
santiagocardoso <santcar7@gmail.com>
2023-10-10 11:59:04 -0300

    final aula lfa 10/10/23

--- sap.py
+++ sap.py
@@ -10,12 +10,20 @@ alfabeto = ['a', 'b']
 estados = ["Q0", "Q1", "Q2"]
 
 inicial = "Q0"
-finais = {"Q2"}
+finais = ["Q2"]
 
 transicoes = {"Q0":{'a':"Q1", 'b':"Q2"}, "Q1":{'a':"Q1", 'b':"Q2"}}
 
 palavra = "aab"
 
+atual = inicial
+
 for e in palavra:
     if transicoes[inicial][e]:
-        print(f"Estados atual = {transicoes[inicial][e]}")
+        print(f"Estado atual = {transicoes[atual][e]}")
+        atual = transicoes[atual][e]
+
+if atual in finais:
+    print("Palavra válida!")
+else:
+    print("Palavra não é válida!")
